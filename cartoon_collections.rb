2cc0713878255419dcae5441b986f072a35dfe19
Teddy Milford <teddy.milford@gmail.com>
2022-06-27 10:20:22 -0600

    commit tree 4393330f69562249eae8a096b08a61a0bbd73790

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)
+  arr.map.with_index { |d, index| puts "#{index}. #{d}" }
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map.with_index do |cap, index|
+   split = planeteer_calls[index - 1].split()
+   first_letter = split[0].upcase
+   rest = split.slice(1).join()
+   total = "#{first_letter}#{rest}!"
+  end
+end
+
+end
+
+def long_planeteer_calls # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese # code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = %w[cheddar gouda camembert]
 end
